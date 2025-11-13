3e5c235e9589ff383d887f265c75d21f642b4985
Lavanya Sharma <127662996+lava918@users.noreply.github.com>
2025-11-13 21:28:14 +0530

    Update page.tsx

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -16,7 +16,7 @@ export default function SkillsPage() {
     },
     {
       title: "Core Competencies",
-      items: ["Git", "GitHub", "Debugging", "Problem Solving", "Analytical Thinking", "Collaboration", "Adaptability", "Project Leadership"],
+      items: ["Git", "GitHub", "Debugging", "Linux", "Problem Solving", "Analytical Thinking", "Collaboration", "Adaptability", "Project Leadership"],
     },
     {
       title: "Machine Learning & Data Science",
